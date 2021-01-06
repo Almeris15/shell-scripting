5c21787f72059472e9ceec2b2870197c7fefa7f6
Arthur Coulon <arthur.coulon@edu.devinci.fr>
2021-01-06 14:52:41 +0100

    Modification du script de l'exercice 4

--- exercise-4/salutation.sh
+++ exercise-4/salutation.sh
@@ -1,1 +1,9 @@
-code exercice 4
+#! bin/bash
+
+if argument_1 argument_2; then
+	echo "hello , argument_1 argument_2, have a nice day!"
+	return 0;
+else 
+	echo "Usage: ./salutation.sh firstname lastname";
+	return 1;
+fi
