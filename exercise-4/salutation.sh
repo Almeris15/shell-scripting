6805a7c11aa5e79f1938fac833697d4e596625ad
BayTeo <teo.bay@edu.devinci.fr>
2021-01-07 22:32:32 +0100

    Modification de l'exercice 4

--- exercise-4/salutation.sh
+++ exercise-4/salutation.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 if [ -z != $1 ] && [ -z != $2 ]; then 
-	echo "hello $1 $2, have a nice day!"
-else 
+	echo "Hello $1 $2, have a nice day !"
+else
 	echo "Usage: ./salutation.sh firstname lastname"
 fi
+echo Usage: ./salutation.sh firstname lastname
