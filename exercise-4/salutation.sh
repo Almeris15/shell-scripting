2d1948de942ea1a8677c9d3fe3b58096a1b5cd10
Arthur Coulon <arthur.coulon@edu.devinci.fr>
2021-01-07 23:03:19 +0100

    Résolution de l'exercice 4

--- exercise-4/salutation.sh
+++ exercise-4/salutation.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-if [ -z != $1 ] && [ -z != $2 ]; then 
-	echo "Hello $1 $2, have a nice day !"
+if [ -z != $1 ] && [ -z != $2 ]; then
+	if [ -z != $3 ]; then
+		echo "Usage: ./salutation.sh firstname lastname"
+	else
+		echo "Hello $1 $2, have a nice day !"
+	fi
 else
 	echo "Usage: ./salutation.sh firstname lastname"
 fi
-echo Usage: ./salutation.sh firstname lastname
