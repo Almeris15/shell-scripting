85fcb96afeeea12d868daa1f4ad02b6d3ac602bf
Arthur Coulon <arthur.coulon@edu.devinci.fr>
2021-01-07 20:11:37 +0100

    code de l'exercice 7 mais il manque une condition

--- exercise-7/my_ansible_advanced.sh
+++ exercise-7/my_ansible_advanced.sh
@@ -1,1 +1,34 @@
-code exercice 7
+#! /bin/bash
+
+if [ -z $1 ]; then
+
+	mkdir documents documents/gamez documents/images documents/work documents/work/code documents/work/plannings
+	touch documents/work/plannings/april.xlsx documents/work/plannings/february.xlsx documents/work/plannings/january.xlsx documents/work/plannings/march.xlsx meeting_notes.txt documents/gamez/csgo.exe
+
+elif [ -z != $1 ]; then
+	mkdir documents
+	mkdir documents/gamez
+	mkdir documents/images
+	mkdir documents/work
+	mkdir documents/work/code
+	mkdir documents/work/plannings
+	mkdir documents/$1
+	mkdir documents/$1/gamez
+	mkdir documents/$1/images
+	mkdir documents/$1/work
+	mkdir documents/$1/work/code
+	mkdir documents/$1/work/plannings
+	touch documents/work/plannings/april.xlsx
+	touch documents/work/plannings/february.xlsx 
+	touch documents/work/plannings/january.xlsx 
+	touch documents/work/plannings/march.xlsx 
+	touch meeting_notes.txt
+	touch documents/gamez/csgo.exe
+	touch documents/$1/gamez/csgo.exe
+	touch documents/$1/work/plannings/april.xlsx
+	touch documents/$1/work/plannings/february.xlsx
+	touch documents/$1/work/plannings/january.xlsx
+	touch documents/$1/work/plannings/march.xlsx
+else
+	echo "./my_ansible_advanced.sh [username]"
+fi
