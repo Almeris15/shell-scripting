71a152832c3913a40e6d130ea69c0d66c63cc84e
BayTeo <teo.bay@edu.devinci.fr>
2021-01-07 19:24:18 +0100

    Modification de l'exercice 6

--- exercise-6/my_ansible.sh
+++ exercise-6/my_ansible.sh
@@ -1,8 +1,4 @@
-#! bin/bash
+#! /bin/bash
 
-mkdir documents
-touch meeting_notes.txt
-mkdir documents/gamez documents/images documents/work
-touch csgo.exe
-mkdir documents/work/code documents/work/plannings
-touch documents/work/plannings/april.xlsx documents/work/plannings/february.xlsx documents/work/plannings/january.xlsx documents/work/plannings/march.xlsx
+mkdir documents documents/gamez documents/images documents/work documents/work/code documents/work/plannings
+touch documents/work/plannings/april.xlsx documents/work/plannings/february.xlsx documents/work/plannings/january.xlsx documents/work/plannings/march.xlsx meeting_notes.txt documents/gamez/csgo.exe
