990ec920ed24d12bc6280e280547501e2dd8665e
Arthur Coulon <arthur.coulon@edu.devinci.fr>
2021-01-07 23:21:56 +0100

    Résolution de l'exercice 5

--- exercise-5/salutation_advanced.sh
+++ exercise-5/salutation_advanced.sh
@@ -1,1 +1,17 @@
-code exercice 5:
+#! /bin/bash
+
+if [ -z != $1 ] && [ -z != $2 ]; then
+	if [ -z != $3 ]; then
+		echo "Usage: ./salutation.sh firstname lastname"
+		exit 1
+	elif [ $1 = $2 ]; then
+		echo "Firstname and lastname cannot be the same!"
+		exit 2
+	else 
+		echo "Hello $1 $2, have a nice day !"
+		exit 0
+	fi
+else
+	echo "Usage: ./salutation.sh firstname lastname"
+	exit 1
+fi
